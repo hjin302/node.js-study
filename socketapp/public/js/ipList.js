e77be0a57c0d6cc44049a72aae1c71d144cd3184
hjin302 <enro302@naver.com>
2020-07-28 18:02:55 +0900

    0728 - 웹소켓 ...

---
+++ socketapp/public/js/ipList.js
@@ -0,0 +1,11 @@
+//ES5 (2015년 부터 지원)
+class IpList extends React.Component{
+    //화면에 표현할 코드
+    render(){ 
+        return (
+            <select>
+                <option>192.168.10.3</option>
+            </select>
+        );
+    }
+}
